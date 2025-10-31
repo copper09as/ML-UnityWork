3e38c8618f178a17bcc1f752d4726b1392d296bb
copper <494699676@qq.com>
2025-10-31 17:18:19 +0800

    训练数据

--- Assets/Script/Character/CharacterMove.cs
+++ Assets/Script/Character/CharacterMove.cs
@@ -13,29 +13,29 @@ public class Character2DAgent : Agent
 
     [Header("Target Settings")]
     public Transform targetTransform;
+    public Transform initPositionTransform;
+    public Transform deadPositionTransform;
     public float reachRadius = 0.5f;
-
+    
     // ¶¯×÷»º´æ
     private float moveInput = 0f;
     private float turnInput = 0f;
 
     [Header("Debug Settings")]
     public bool debugLogs = true;
-    private Vector2 initPosition;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        initPosition = transform.position;
     }
     public override void OnEpisodeBegin()
     {
-        if (this.transform.localPosition.y < 0)
+        if (this.transform.localPosition.y < deadPositionTransform.position.y)
         {
             this.rb.angularVelocity = 0;
             this.rb.velocity = Vector2.zero;
         }
-        targetTransform.localPosition = new Vector2(Random.Range(-8,8), 1.5f);
-        transform.position = initPosition;
+        targetTransform.localPosition = new Vector2(Random.Range(-8,8), targetTransform.position.y);
+        transform.position = initPositionTransform.position;
 
     }
     public override void CollectObservations(VectorSensor sensor)
@@ -47,18 +47,17 @@ public class Character2DAgent : Agent
     public float forceMultiplier = 2;
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
-        Vector2 controlSignal = Vector2.zero;
-        controlSignal.x = actionBuffers.ContinuousActions[0];
-        rb.velocity = new Vector2(controlSignal.x * forceMultiplier, rb.velocity.y);
+        int dir = actionBuffers.DiscreteActions[0]-1;
+        rb.velocity = new Vector2(dir * forceMultiplier, rb.velocity.y);
         // Rewards
         float distanceToTarget = Vector2.Distance(this.transform.localPosition,targetTransform.localPosition);
-        if (distanceToTarget < 1f)
+        if (distanceToTarget < 2f)
         {
             SetReward(100.0f);
             Debug.Log("Reward"+distanceToTarget.ToString());
             EndEpisode();
         }
-        else if (this.transform.localPosition.y < 0)
+        else if (this.transform.localPosition.y < deadPositionTransform.position.y)
         {
             Debug.Log(transform.localPosition.y.ToString());
             AddReward(-100);
@@ -69,7 +68,5 @@ public class Character2DAgent : Agent
     
     public override void Heuristic(in ActionBuffers actionsOut)
     {
-        var continuousActionsOut = actionsOut.ContinuousActions;
-        continuousActionsOut[0] = Input.GetAxis("Horizontal");
     }
 }
